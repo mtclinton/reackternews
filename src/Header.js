9cbdaccf42d15fddf8415fef74e9375988bf6d16
mtclinton <maxtclinton@gmail.com>
2021-05-09 12:21:31 -0400

    header work

--- src/Header.js
+++ src/Header.js
@@ -3,17 +3,37 @@ import React from 'react';
 function Header() {
     return (
         <div className="Header">
-            <h1>Hacker News Clone</h1>
-            <div className="nav-link">
-                <a href="/top" className="active">
-                    Top Stories
-                </a>
-                <a href="/new" className="active">
-                    Latest Stories
-                </a>
-                <a href="/best" className="active">
-                    Best Stories
-                </a>
+            <div style={{background: "#ff6600"}}>
+                <div style={{padding: "2px"}}>
+                    <span style={{width: "18px", paddingRight: "4px"}}>
+                        <a href={"https://news.ycombinator.com"}>
+                            <img
+                                src={"https://news.ycombinator.com/y18.gif"}
+                                style={{width: "18px", height: "18px",border: "1px white solic"}} />
+                        </a>
+
+                    </span>
+                    <span style={{lineHeight: "12px", height: "10px"}}>
+                        <span className="pagetop">
+                            <b className="hnname"><a href="news">Hacker News</a></b>
+                            <a href="newest">new</a> |
+                            <a href="front">past</a> |
+                            <a href="newcomments">comments</a> |
+                            <a href="ask">ask</a> |
+                            <a href="show">show</a> |
+                            <a href="jobs">jobs</a> |
+                            <a href="submit">submit</a>
+                        </span>
+
+                    </span>
+                    <span style={{textAlign: "right",paddingRight:"4px"}}>
+                        <span className="pagetop">
+                              <a href="login?goto=news">login</a>
+                          </span>
+                    </span>
+
+                </div>
+
             </div>
         </div>
     );
