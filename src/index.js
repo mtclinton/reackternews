eb68180221b23761fd6b46f3d9b07e4e3fbebb23
mtclinton <maxtclinton@gmail.com>
2021-05-10 15:30:36 -0400

    added react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+
+} from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+
+        <Switch>
+            <Route path="/about">
+                <App />
+            </Route>
+            <Route path="/topics">
+                <App />
+            </Route>
+            <Route path="/">
+                <App />
+            </Route>
+        </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
