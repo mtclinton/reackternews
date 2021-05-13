6ee5e852afa11704728dd5a726ecacf88ca0d7a3
mtclinton <maxtclinton@gmail.com>
2021-05-12 21:00:46 -0400

    working on pagination

--- src/index.js
+++ src/index.js
@@ -13,20 +13,38 @@ ReactDOM.render(
     <Router>
 
         <Switch>
+            <Route path="/ask/:page">
+                <App type={"ask"}/>
+            </Route>
             <Route path="/ask">
                 <App type={"ask"}/>
+            </Route>
+            <Route path="/show/:page">
+                <App type={"show"}/>
             </Route>
             <Route path="/show">
                 <App type={"show"}/>
             </Route>
+            <Route path="/jobs/:page">
+                <App type={"job"}/>
+            </Route>
             <Route path="/jobs">
                 <App type={"job"}/>
+            </Route>
+            <Route path="/newest/:page">
+                <App type={"new"}/>
             </Route>
             <Route path="/newest">
                 <App type={"new"}/>
             </Route>
+            <Route path="/best/:page">
+                <App type={"best"}/>
+            </Route>
             <Route path="/best">
                 <App type={"best"}/>
+            </Route>
+            <Route path="/news/:page">
+                <App type={"top"}/>
             </Route>
             <Route path="/news">
                 <App type={"top"}/>
